fadb04ba32a1e05a7929e36d4f578564a49e0df7
agamyo168 <agamyo168@gmail.com>
2021-07-24 16:54:40 +0200

    update to main.js and removed h1 from html

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // setup canvas
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -12,3 +11,90 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+function Ball (x, y, velX, velY,color, size) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.size = size;
+    this.velX = velX;
+    this.velY = velY;
+   
+  }
+  Ball.prototype.draw = function()
+  {
+    ctx.beginPath(); //To state that we want to start drawing a shape to paper
+    ctx.fillStyle = this.color; //define what color we want the shape to be
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI); //(x,y) center size = radius, starting angle = 0 and ending angle = 360
+    ctx.fill();//finish drawing that we started with beginPath and color it with fillStyle color
+  }
+  
+
+  let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10,20);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+    ,true
+  );
+
+  balls.push(ball);
+}
+Ball.prototype.update = function()
+  {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+      }
+    
+      if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+      }
+    
+      this.x += this.velX;
+      this.y += this.velY;
+      
+  }
+  Ball.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        }
+      }
+    }
+  }
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+    ctx.fillRect(0, 0, width, height);
+  
+    for (let i = 0; i < balls.length; i++) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+  
+    requestAnimationFrame(loop);
+    console.log('running');
+  }
+
+loop();
